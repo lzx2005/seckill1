9c696c6a82f58b9d4eef84fb63f82d19d570cb76
lzx2005 <978744976@qq.com>
2016-05-26 17:20:00 +0800

    修补SQL错误

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -3,17 +3,17 @@ CREATE DATABASE seckill;
 use seckill;
 --秒杀商品表
 CREATE TABLE seckill(
-  'seckill_id' bigint NOT NULL AUTO_INCREMENT COMMENT='商品库存ID',
-  'name' varchar(120) NOT NULL COMMENT='商品标题',
-  'number' int NOT NULL COMMENT='商品剩余总量',
-  'start_time' TIMESTAMP NOT NULL COMMENT='开始时间',
-  'end_time' TIMESTAMP NOT NULL COMMENT='结束时间',
-  'create_time' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT='创建时间',
+  `seckill_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品库存ID',
+  `name` varchar(120) NOT NULL COMMENT '商品标题',
+  `number` int NOT NULL COMMENT '商品剩余总量',
+  `start_time` TIMESTAMP NOT NULL COMMENT '开始时间',
+  `end_time` TIMESTAMP NOT NULL COMMENT '结束时间',
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (seckill_id),
   key idx_start_time(start_time),
   key idx_end_time(end_time),
   key idx_create_time(create_time)
-)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀商品表'
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀商品表';
 
 --初始化数据
 INSERT INTO
@@ -27,10 +27,10 @@ VALUES
 --秒杀结果表
 --信息记录表
 CREATE TABLE success_killed(
-  `seckill_id` bigint NOT NULL COMMENT='商品库存',
+  `seckill_id` bigint NOT NULL COMMENT '商品库存',
   `phone` bigint NOT NULL COMMENT '用户手机号码',
   `state` tinyint NOT NULL DEFAULT -1 COMMENT '状态:-1无效,0成功,1已付款',
-  'create_time' TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT='创建时间',
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY(seckill_id,phone),/*联合主键*/
   key idx_create_time(create_time)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
